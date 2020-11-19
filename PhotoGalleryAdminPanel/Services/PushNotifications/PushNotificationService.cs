f1375217658e5746b416629baa1ecfff1086fca2
Oleh Tymoshenko <olegtymoshenko2000@gmail.com>
2020-11-19 12:31:27 +0200

    All works

--- PhotoGalleryAdminPanel/Services/PushNotifications/PushNotificationService.cs
+++ PhotoGalleryAdminPanel/Services/PushNotifications/PushNotificationService.cs
@@ -9,8 +9,8 @@ namespace PhotoGalleryAdminPanel.Services.PushNotifications
 {
 	public class PushNotificationService : IPushNotificationService
 	{
-		private const string ROOT_PATH_FOR_TOPICS = "/topics";
-		private const string DEFAULT_NOTIFICATION_IMAGE_URL = "https://www.softmagazin.ru/upload/iblock/b74/b749606ae3efde0d569795cba1f495ca.png";
+		private const string ROOT_PATH_FOR_TOPICS = "/topics/";
+		private const string DEFAULT_NOTIFICATION_IMAGE_URL = "https://cdn.auth0.com/blog/get-started-ionic/logo.png";
 
 		public FirebaseMessaging messagingFB { get; set; }
 
@@ -20,7 +20,7 @@ namespace PhotoGalleryAdminPanel.Services.PushNotifications
 		{
 			var msg = new Message()
 			{
-				Topic = ROOT_PATH_FOR_TOPICS + (notification.Topic ?? "/all"),
+				Topic = ROOT_PATH_FOR_TOPICS + (notification.Topic ?? "all"),
 				Notification = new Notification()
 				{
 					Title = notification.Title ?? "Photo gallery notification",
